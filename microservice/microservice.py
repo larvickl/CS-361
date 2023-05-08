9a31a912c082e42d6d9d395d9f1dc81240912707
larvickl <107569872+larvickl@users.noreply.github.com>
2023-05-07 21:36:34 -0700

    Update microservice.py

--- microservice/microservice.py
+++ microservice/microservice.py
@@ -1,5 +1,5 @@
-from flask import Flask, jsonify, request
 import requests
+from flask import Flask, request
 
 app = Flask(__name__)
 
@@ -7,22 +7,22 @@ app = Flask(__name__)
 def get_observations():
     park_name = request.args.get('park_name')
     if not park_name:
-        return jsonify({'error': 'No park name provided.'}), 400
+        return 'Error: Park name not specified.'
 
-    url = 'https://api.inaturalist.org/v1/observations'
-    params = {'q': park_name, 'order_by': 'observed_on'}
-    response = requests.get(url, params=params)
-    observations = response.json()['results']
+    url = f'https://api.inaturalist.org/v1/observations'
+    params = {
+        'q': park_name,
+        'order_by': 'observed_on',
+        'has_photos': True
+    }
 
-    filtered_observations = [obs for obs in observations if
-                             obs.get('observation_photos_count', 0) > 0 and obs.get('taxon') and obs['taxon'].get(
-                                 'common_name')]
-
-    data = [{'The Common Name': obs['taxon']['common_name'], 'The URI of the Observation': obs['uri'],
-                       'Observed Date': obs['observed_on'],
-                       'A Photo of the Observation': obs['photos'][0]['medium_url']} for obs in filtered_observations]
-
-    return jsonify(data)
+    try:
+        response = requests.get(url, params=params)
+        response.raise_for_status()
+        observations = response.json()['results']
+        return {'observations': observations}
+    except requests.exceptions.RequestException as e:
+        return f'Error: {e}'
 
 if __name__ == '__main__':
     app.run()
